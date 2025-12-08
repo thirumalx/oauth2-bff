d29d7b5b04074e810266001a943b599568abb083
Thirumal M <racetortoise@gmail.com>
2025-12-08 11:13:45 +0530

    added encode

--- src/main/java/com/thirumal/service/AuthService.java
+++ src/main/java/com/thirumal/service/AuthService.java
@@ -2,13 +2,19 @@ package com.thirumal.service;
 
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
-
+/**
+ * @author ThirumalM
+ */
 @Service
 public class AuthService {
+	
+	Logger logger = LoggerFactory.getLogger(AuthService.class);
 
 	private final ClientRegistrationRepository repo;
 	
@@ -27,21 +33,19 @@ public class AuthService {
                 .replace("{registrationId}", reg.getRegistrationId());
 
         // Resolve scopes
-        String scopes = reg.getScopes()
-                .stream()
+        String scopes = reg.getScopes().stream()
                 .collect(Collectors.joining(" "));
 
-        String authorizeUrl = UriComponentsBuilder
+        return UriComponentsBuilder
                 .fromUriString(reg.getProviderDetails().getAuthorizationUri())
                 .queryParam("response_type", "code")
                 .queryParam("client_id", reg.getClientId())
                 .queryParam("redirect_uri", redirectUri)
                 .queryParam("scope", scopes)
                 .queryParam("state", state)
+                .encode()
                 .build()
                 .toUriString();
-
-        return authorizeUrl;
     }
 
 }
